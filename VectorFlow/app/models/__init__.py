b09f6597fdf6dd340b4f2e3207096fc4819987cd
TechPertz <reetnandy@gmail.com>
2025-03-25 13:59:29 -0400

    ignore - init files

--- VectorFlow/app/models/__init__.py
+++ VectorFlow/app/models/__init__.py
@@ -1,11 +1,13 @@
 from .schemas import (
     Chunk, ChunkBase, ChunkCreate, ChunkMetadata,
     Document, DocumentBase, DocumentCreate, DocumentMetadata,
-    Library, LibraryBase, LibraryCreate, LibraryMetadata
+    Library, LibraryBase, LibraryCreate, LibraryMetadata,
+    BatchTextInput
 )
 
 __all__ = [
     "Chunk", "ChunkBase", "ChunkCreate", "ChunkMetadata",
     "Document", "DocumentBase", "DocumentCreate", "DocumentMetadata",
-    "Library", "LibraryBase", "LibraryCreate", "LibraryMetadata"
+    "Library", "LibraryBase", "LibraryCreate", "LibraryMetadata",
+    "BatchTextInput"
 ] 
